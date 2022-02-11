ea5e72cdc2d5f28c96c2cce63f3b8e771b2cb8e1
codepanther <pezechigozirim@gmail.com>
2022-02-11 14:54:27 +0100

    Create and style the Navbar

---
+++ src/components/Topbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Search, Person, Chat, Notifications } from '@material-ui/icons'
+import './topbar.css';
+
+const Topbar = () => {
+  return (
+    <section className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Sociala</span>
+      </div>
+
+      <div className="topbarCenter">
+        <div className="searchBar">
+          <Search className="searchIcon" />
+          <input type="text" className="searchInput" placeholder="Search a for friend, post or video" />
+        </div>
+      </div>
+
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <div className="topbarImgContainer">
+          <img src="/assets/people/1.jpg" alt="profile-pic" className="topbarImg" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Topbar
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Topbar from '../components/Topbar';
+import './home.css';
+
+const Home = () => {
+  return (
+    <Topbar />
+  )
+}
+
+export default Home;

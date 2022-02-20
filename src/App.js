1db4ea62a2e170a3bd480d21179f24def20763f9
codepanther <pezechigozirim@gmail.com>
2022-02-20 12:28:24 +0100

    Implement Only loggedInUsers can access homepage.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { Home, Login, Profile, Register } from './pages';
-import { Route, Routes } from 'react-router-dom';
-import './style.css';
+import React from "react";
+import { Home, Login, Profile, Register } from "./pages";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./style.css";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { data: userLoggedIn } = useSelector((state) => state.loginReducer);
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/profile/:username' element={<Profile />} />
+        <Route
+          path="/"
+          element={
+            userLoggedIn.username ? <Home /> : <Navigate replace to="/login" />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            userLoggedIn.username ? <Navigate replace to="/" /> : <Login />
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
       </Routes>
-      
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

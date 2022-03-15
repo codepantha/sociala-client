2d81efd86c04a30f3d31e6dfacd85b4c658aec5e
codepanther <pezechigozirim@gmail.com>
2022-03-15 12:36:09 +0100

    Sort posts from newest to oldest

--- src/redux/posts/posts.js
+++ src/redux/posts/posts.js
@@ -7,15 +7,20 @@ const GET_PROFILE_POSTS_REQUEST = 'GET_PROFILE_POSTS_REQUEST';
 const GET_PROFILE_POSTS_SUCCESS = 'GET_PROFILE_POSTS_SUCCESS'
 const GET_PROFILE_POSTS_FAILED = 'GET_PROFILE_POSTS_FAILED'
 
+const getMostRecent = async (data) => {
+  return data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+}
+
 export const getLoggedInUserPosts = (userLoggedIn) => (dispatch) => {
   dispatch({ type: GET_POSTS_REQUEST })
 
   const fetchResults = async () => {
     try {
       const res = await axios.get(`posts/timeline/${userLoggedIn._id}`);
-      dispatch({ type: GET_POSTS_SUCCESS, payload: res.data })
+      const data = await getMostRecent(res.data)
+      dispatch({ type: GET_POSTS_SUCCESS, payload: data })
     } catch (err) {
-      dispatch({ type: GET_POSTS_FAILED, payload: 'something bad happened.'})
+      dispatch({ type: GET_POSTS_FAILED, payload: 'something bad happened.' })
     }
   }
   fetchResults();
@@ -27,7 +32,8 @@ export const getProfilePosts = (username) => (dispatch) => {
   const fetchResults = async () => {
     try {
       const res = await axios.get(`/posts/${username}/all`)
-      dispatch({ type: GET_PROFILE_POSTS_SUCCESS, payload: res.data })
+      const data = await getMostRecent(res.data)
+      dispatch({ type: GET_PROFILE_POSTS_SUCCESS, payload: data })
     } catch (err) {
       dispatch({ type: GET_PROFILE_POSTS_FAILED, payload: res.data })
     }
@@ -41,19 +47,19 @@ const initialState = {
   error: null
 }
 const postsReducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case GET_POSTS_REQUEST:
       return { ...initialState, loading: true }
-  case GET_PROFILE_POSTS_REQUEST:
-    return { ...initialState, loading: true }
+    case GET_PROFILE_POSTS_REQUEST:
+      return { ...initialState, loading: true }
     case GET_POSTS_SUCCESS:
       return { ...initialState, loading: false, posts: action.payload }
     case GET_PROFILE_POSTS_SUCCESS:
       return { ...initialState, loading: false, posts: action.payload }
     case GET_POSTS_FAILED:
-      return { loading: false, posts: [], error: action.payload}
+      return { loading: false, posts: [], error: action.payload }
     case GET_PROFILE_POSTS_FAILED:
-      return { loading: false, posts: [], error: action.payload}
+      return { loading: false, posts: [], error: action.payload }
 
     default:
       return state

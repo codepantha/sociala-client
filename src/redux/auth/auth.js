a65de8b774e010487dabbb8c96aef9a82620dbe9
codepanther <pezechigozirim@gmail.com>
2022-03-20 07:42:06 +0100

    Update follow and unfollow user in redux state

--- src/redux/auth/auth.js
+++ src/redux/auth/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST';
-const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS';
-const USER_LOGIN_FAILED = 'USER_LOGIN_FAILED';
+const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
+const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+const USER_LOGIN_FAILED = "USER_LOGIN_FAILED";
 
 export const login = (user) => (dispatch) => {
   dispatch({ type: USER_LOGIN_REQUEST });
@@ -14,38 +14,53 @@ export const login = (user) => (dispatch) => {
       });
       dispatch({ type: USER_LOGIN_SUCCESS, payload: res.data });
     } catch (e) {
-      dispatch({ type: USER_LOGIN_FAILED, payload: "Ooops! Something bad happened!" });
+      dispatch({
+        type: USER_LOGIN_FAILED,
+        payload: "Ooops! Something bad happened!"
+      });
     }
-  }
+  };
   executeLogin();
-}
-
-// export const followUser = (id) => (dispatch) => {
-//   dispatch
-// }
+};
 
 const initialScale = {
   loading: true,
   data: {},
-  error: null,
-}
+  error: null
+};
 
 const loginReducer = (state = initialScale, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { loading: true, ...state }
+      return { loading: true, ...state };
     case USER_LOGIN_SUCCESS:
-      return { loading: false, data: action.payload }
+      return { loading: false, data: action.payload };
     case USER_LOGIN_FAILED:
-      return { loading: false, data: action.payload }
-    case 'FOLLOW_USER':
-      return { ...state, data: {...state.data, following: [...state.data.following, action.payload]}}
-    case 'UNFOLLOW_USER':
-      return { ...state, data: { ...state.data, following: [...state.data.following.filter(user_id => user_id !== action.payload)]} }
+      return { loading: false, data: action.payload };
+    case "FOLLOW_USER":
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          following: [...state.data.following, action.payload]
+        }
+      };
+    case "UNFOLLOW_USER":
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          following: [
+            ...state.data.following.filter(
+              (user_id) => user_id !== action.payload
+            )
+          ]
+        }
+      };
 
     default:
       return state;
   }
-}
+};
 
 export default loginReducer;
